4604a955424321067a62a87cabd7933b5940f56f
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-03-12 20:26:05 +0700

    sidebar

---
+++ srcs/requirements/frontend/src/pages/smallPages/Settings.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="settings">
+        <div class="sidebar">
+            <SideBar />
+        </div>
+
+        <div class="setup">
+            <h1>settings</h1>
+            <p>Set up your shit</p>
+        </div>
+        
+    </div>
+    
+</template>
+<script setup>
+import SideBar from '../../components/SideBar.vue';
+</script>
+
+<style scoped>
+.sidebar {
+	display: flex; 
+	/* flex-direction: column; */
+}
+.setup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.settings {
+    flex: 1 1 0;
+}
+</style>
