686e1bb0e986428a1a3c87e87b2e7a601b25febc
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-03-26 00:44:53 +0700

    got it, bknd and frnts just two stipid bastards whre one is sending whole profile list and and another just tale everythinggit add .git add .git add .

--- srcs/requirements/backend/project/apps/intrauth/admin.py
+++ srcs/requirements/backend/project/apps/intrauth/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 
-from .models import Profile
 admin.site.register(CustomUser)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'wins', 'losses')
+    list_display = ('display_name','email', 'wins', 'losses', 'email')
+    
+    # def user_email(self, obj): # this, and many isers from prifile
+    #     return obj.user.email # one to one link
+    # user_email.short_description = 'Email'
+    
--- srcs/requirements/frontend/src/pages/smallPages/Profile.vue
+++ srcs/requirements/frontend/src/pages/smallPages/Profile.vue
@@ -5,18 +5,49 @@
         </div>
 
         <div class="about">
-            <h1>profile</h1>
-            <p>Here ypu can write some bullshit</p>
+            <h1>My details</h1>
+            <span class="fieldName">display name</span>
+            <p>user: {{ user?.username }} </p>
+            <span class="input"><input v-model="message" placeholder="change me"></span>
+            <!-- <span class="fieldName">email</span> -->
+            <p>Email {{ user?.email }} </p>
+            <!-- <span class="input"><input v-model="message" placeholder="just your email"></span> -->
+            <span class="fieldName">friends</span>
+            <span class="input"><input v-model="message" placeholder="not changable"></span>
+            <span class="fieldName">win</span>
+            <span class="input"><input v-model="message" placeholder="from database"></span>
+            <span class="fieldName">lose</span>
+            <span class="input"><input v-model="message" placeholder="from database"></span>
         </div>
         
     </div>
     
 </template>
 <script setup>
+import axios from 'axios'
+import { onMounted,ref } from 'vue'
+
 import SideBar from '../../components/SideBar.vue';
+
+
+const user = ref(null)
+const profileData = async () => {
+    try {
+        const response = await axios.get('api/profiles/me/', {
+            withCredentials: true
+        })
+        user.value = response.data
+        console.log("user value is:", user.value);
+    } catch (error) {
+        console.error('error happends', error);
+    }
+}
+onMounted(() => {
+    profileData()
+})
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .sidebar {
 	display: flex; 
 	/* flex-direction: column; */
@@ -26,6 +57,7 @@ import SideBar from '../../components/SideBar.vue';
   flex-direction: column;
   align-items: center;
   padding: 20px;
+//   position: ;
 }
 
 .profile {
