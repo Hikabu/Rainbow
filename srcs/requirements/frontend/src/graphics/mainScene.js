697ae69599712e2dad32a7217abff93d5141a6c3
1Clavero <arturoclavero01@gmail.com>
2025-04-13 15:16:04 +0700

    feat: add Three.js integration

---
+++ srcs/requirements/frontend/src/graphics/mainScene.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three'
+
+export default function initThreeScene(canvas) {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
+  const renderer = new THREE.WebGLRenderer({ canvas })
+
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+  camera.position.z = 5
+
+  const geometry = new THREE.BoxGeometry()
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  const animate = function () {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
+
+  animate()
+}
