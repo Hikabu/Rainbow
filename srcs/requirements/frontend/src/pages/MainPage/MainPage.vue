1912293092c8ba1bd4555c6e38b855629c498c6f
1Clavero <arturoclavero01@gmail.com>
2025-04-13 15:16:04 +0700

    feat: add Three.js integration

--- srcs/requirements/frontend/src/pages/MainPage/MainPage.vue
+++ srcs/requirements/frontend/src/pages/MainPage/MainPage.vue
@@ -2,21 +2,24 @@
     <div class="mainpage container-fluid p-0">
         <div class="d-flex row-flex">
             <SideBar />
-                <div class="about col-md-10 p-5 flex-grow-1 position-relative">
-                <!-- </div> -->
-
-                <div class="profile">
-                    <h1>MainPage</h1>
-                    <p>Fridge is cool when you are in hell</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+      		<div class="about col-md-10 p-0 flex-grow-1 position-relative">
+                <!-- Three.js-->
+				<canvas ref="threeCanvas" class="three-canvas"></canvas>
+      		</div>
+         </div>
+    </div>    
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
 import SideBar from '../../components/SideBar.vue';
+import initThreeScene from '../../graphics/mainScene.js'
+
+const threeCanvas = ref(null)
+onMounted(() => {
+  initThreeScene(threeCanvas.value)
+})
+
 </script>
 
 <style scoped>
@@ -34,6 +37,11 @@ import SideBar from '../../components/SideBar.vue';
     min-height: 100vh;
     background: rgb(10,42,54);
     background: linear-gradient(90deg, rgba(10,42,54,1) 0%, rgba(150,0,0,1) 60%, rgba(120,71,0,1) 94%);
+}
 
+.three-canvas {
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 </style>
