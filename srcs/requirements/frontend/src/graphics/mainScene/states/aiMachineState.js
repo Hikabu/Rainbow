2c3ac99f0e4ebc4526a85d1253cb951f027106fe
1Clavero <arturoclavero01@gmail.com>
2025-04-14 13:31:07 +0700

    chore: adjust UI styles

---
+++ srcs/requirements/frontend/src/graphics/mainScene/states/aiMachineState.js
@@ -0,0 +1,105 @@
+
+
+import { State } from '../../core/stateManager/States';
+import { MeshSubState , CssSubState} from '../../core/stateManager/SubStatesExtends';
+import { screenMaterial } from '../objects/simpleAssets';
+import { screenSurface, center, object, partIndex, surfaceIndex } from '../objects/machines/aiMachineObj';
+import { StartScreen } from '../overlays/divs/start'
+import { End } from '../overlays/divs/end';
+import { pongGame, startPongGame } from '../overlays/scenes/pong-game/Game';		
+import { AlertManager } from '../overlays/alerts/Alerts';
+import { StateManager } from '../../core/stateManager/StateManager';
+
+const divStart = new StartScreen('white', "START GAME");
+
+const restScreen = new CssSubState(
+	"rest",
+	object,
+	partIndex,
+	surfaceIndex,
+	divStart.div,
+	0,
+	()=>{
+		divStart.enter();
+		divStart.enterButton.element.style.visibility = "hidden";
+	},
+	null,
+	null,
+	()=>{ divStart.resize();},
+	(event)=> { return divStart.keyHandler(event);},
+	null,
+)
+
+const startScreen = new CssSubState(
+	"start",
+	object,
+	partIndex,
+	surfaceIndex,
+	divStart.div,
+	0,
+	null,
+	()=>{
+		divStart.enterButton.element.style.visibility = "visible";
+	},
+	()=>{ divStart.exit();},
+	()=>{ divStart.resize();},
+	(event)=> { return divStart.keyHandler(event);},
+	()=>{divStart.animate()},
+)
+
+
+const gameScreen = new MeshSubState(
+	"rest", 
+	screenSurface,
+	pongGame,
+	1,
+	()=>{startPongGame("AI")},
+	()=>{
+		new AlertManager().remove_latest_alert("exit_alert");
+	},
+)
+
+const divEnd = new End("white");
+const endScreen = new CssSubState(
+	"end", 
+	object,
+	partIndex,
+	surfaceIndex,
+	divEnd.div,
+	0,
+	()=>{divEnd.enter()},
+	null,
+	()=>{divEnd.exit()},
+	()=>{divEnd.resize()},
+	(event)=> { return divEnd.keyHandler(event);},
+	()=>{divEnd.animate()},
+)
+
+const aiMachineState = new State(
+	"ai game screen", 
+	{
+		pos: [center.x,center.y,center.z],
+		duration: 2,
+		ease: "power2.inOut"
+	}, 
+	[
+		restScreen,
+		startScreen, 
+		gameScreen,
+		endScreen
+	],
+	null,
+	()=>{
+		if (new StateManager().currentState.currentSubstateIndex == "2")
+			{
+				if (create_exit_alert() == "cancelled")
+					return ("cancelled")
+			}
+	},
+	[
+		screenMaterial,
+		pongGame.renderMaterial,
+	],
+)
+
+export {aiMachineState}
--- srcs/requirements/frontend/src/pages/MainPage/MainPage.vue
+++ srcs/requirements/frontend/src/pages/MainPage/MainPage.vue
@@ -1,47 +1,24 @@
+<!-- MainPage.vue -->
 <template>
-    <div class="mainpage container-fluid p-0">
-        <div class="d-flex row-flex">
-            <SideBar />
-      		<div class="about col-md-10 p-0 flex-grow-1 position-relative">
-                <!-- Three.js-->
-				<canvas ref="threeCanvas" class="three-canvas"></canvas>
-      		</div>
-         </div>
-    </div>    
+  <div class="mainpage container-fluid p-0">
+    <div class="d-flex row-flex">
+      <SideBar />
+      <div class="about col-md-10 p-0 flex-grow-1 position-relative">
+        <!-- A container DIV, not a canvas -->
+        <div ref="threeContainer" class="three-container"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import SideBar from '../../components/SideBar.vue';
-import initThreeScene from '../../graphics/mainScene.js'
+import { init } from '../../graphics/index.js'
 
-const threeCanvas = ref(null)
-onMounted(() => {
-  initThreeScene(threeCanvas.value)
+const threeContainer = ref(null)
+
+onMounted(async () => {
+  await init(threeContainer.value) // This is a div now
 })
-
 </script>
-
-<style scoped>
-.sidebar {
-	display: flex; 
-}
-.profile {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-}
-
-.about {
-    min-height: 100vh;
-    background: rgb(10,42,54);
-    background: linear-gradient(90deg, rgba(10,42,54,1) 0%, rgba(150,0,0,1) 60%, rgba(120,71,0,1) 94%);
-}
-
-.three-canvas {
-  width: 100%;
-  height: 100%;
-  display: block;
-}
-</style>
