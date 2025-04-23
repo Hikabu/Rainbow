0863b7c1bf1c041694a429a4048b8fc980c8cdfb
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-04-23 20:25:01 +0700

    feat: add wallet connection

--- srcs/requirements/frontend/src/main.js
+++ srcs/requirements/frontend/src/main.js
@@ -1,4 +1,3 @@
-import '@dzangolab/vue-country-flag-icon/dist/CountryFlag.css';
 import './vendor/normalize.css';
 import './vendor/fonts/lexend_exa/lexend_exa.css';
 import './styles.css';
@@ -6,10 +5,10 @@ import './assets/styles/global.css';
 import 'ant-design-vue/dist/reset.css';
 
 import axios from 'axios';
-// import CountryFlag from '@dzangolab/vue-country-flag-icon';
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import web3Onboard from './components/onboard';
 //routers
 import router from './router';
 
@@ -18,8 +17,7 @@ axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.withCredentials = true;
 
 const app = createApp(App);
-// app.component('CountryFlag', CountryFlag);
 
 //router
-app.use(router);
+app.use(router, web3Onboard);
 app.mount('#app');
