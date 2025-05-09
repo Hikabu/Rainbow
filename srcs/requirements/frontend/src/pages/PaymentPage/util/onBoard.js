dd10c5ca614adff203413fddc7965586379d4ca7
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-05-09 15:35:21 +0700

    feat: set up smart contract

--- srcs/requirements/frontend/src/pages/PaymentPage/util/onBoard.js
+++ srcs/requirements/frontend/src/pages/PaymentPage/util/onBoard.js
@@ -5,42 +5,43 @@ import { ref } from 'vue'
 
 import contractABI from "../contract-abi.json";
 export function onBoard() {
-    const alchemyUrl = import.meta.env.VITE_ALCHEMY_KEY;
+    // const alchemyUrl = import.meta.env.VITE_API_URL;
     const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-
-    const currentAccount = ref(null);
-    const message = ref('');
-    const contractResponse = ref('');
+    let message = ref('');
+    let contractResponse = ref('');
+    let currentAccount = ref(null)
     //taking the node forn alchemy (can not read the data without a node connection so alchemy is a read ony provider )
 
     //class contracts to connect to smart contract 
     //abi content the description of the ocntract 
     //provider - read only access
-    // const readContract = new ethers.Contract(contractAddress, contractABI, provider)
-    const connectWallet = async() => {
-        if (window.ethereum) {
-            try {
-                const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-                currentAccount.value = accounts[0];
-                return currentAccount.value;
-            } catch (error){
-                console.error("Error connecting: ", error);
-             }
-        } else  {
-                alert("Metamask not found!")
-            };
-    };
+    let signer = null;
+    let provider;
 
+    const connectWallet = async() => {
+    if (window.ethereum == null) {
+        alert("Metamask not found!")
+        console.log("MetaMask not installed; using read-only defaults")
+        provider = ethers.getDefaultProvider();
+    } else  {
+        provider = new ethers.BrowserProvider(window.ethereum)
+        signer = await provider.getSigner();
+        currentAccount.value = signer.address;
+        console.log("provider is: ", provider);
+        return currentAccount.value;
+        };
+    };
     const getContract = async() => {
-        const provider = new ethers.JsonRpcProvider(alchemyUrl);
-        const signer = await provider.getSigner();
+        let signer = await provider.getSigner();
         return new ethers.Contract(contractAddress, contractABI, signer);
     };
-
     const writeContract = async() => {
         try{
             const contract = await getContract();
-            const tx = await contract.setMessage(message.value)
+            console.log("what is in the contrsct", contract.interface.fragments.map(f => f.name));
+            console.log("the contract is", contract)
+
+            const tx = await contract.update(message.value)
             await tx.wait();
             alert("transaction successfull")
         } catch (error) {
@@ -49,8 +50,9 @@ export function onBoard() {
     }
     const readContract = async() => {
         try {
+            //message is public tate variable - but solidity already creates the getter function 
             const contract = await getContract();
-            const response = await contract.getMessage();
+            const response = await contract.message();
             contractResponse.value = response; 
         } catch(error) {
             console.error("Read error", error);
@@ -63,11 +65,10 @@ export function onBoard() {
     return {
         loadCurrentMessage,
         connectWallet,
-        // getContract,
         writeContract,
         readContract,
-        currentAccount,
         message,
+        currentAccount,
         contractResponse,
     }
 };
